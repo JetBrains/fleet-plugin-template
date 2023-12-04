e1471111eb350d9d2f9531e3a111e9acf3b34485
Titouan Bion <titouan.bion@jetbrains.com>
2023-12-04 20:29:48 +0100

    Use `fleet-plugins-private-preview` Space project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
         maven("https://packages.jetbrains.team/maven/p/teamcity-rest-client/teamcity-rest-client")
         maven {
-            url = uri("https://packages.jetbrains.team/maven/p/fleet/fleet-sdk")
+            url = uri("https://packages.jetbrains.team/maven/p/fleet-plugins-private-preview/fleet-sdk")
             // only needed until the plugin is public
             credentials {
                 username = settings.providers.gradleProperty("spaceUsername").orNull
