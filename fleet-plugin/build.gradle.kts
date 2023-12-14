3ff4b458b352bad350d8851b0ed4248a6dcc57e6
Titouan Bion <titouan.bion@jetbrains.com>
2023-12-14 12:10:30 +0100

    Upgrade to latest plugin version `0.3.129`

--- fleet-plugin/build.gradle.kts
+++ fleet-plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.20"
-    id("org.jetbrains.fleet-plugin") version "0.3.126"
+    id("org.jetbrains.fleet-plugin") version "0.3.129"
 }
 
 repositories {
