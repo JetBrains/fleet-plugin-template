889ab4b14e4f5c80081d047fb5c7f6f574001770
Titouan Bion <titouan.bion@jetbrains.com>
2024-07-16 23:32:49 +0200

    Upgrade plugin version to 0.5.223`
    - fix regression that made the tooling require `vendorId` or a marketplace token even for `runFleet`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     kotlin("multiplatform") version "1.9.24" apply false
-    id("org.jetbrains.fleet-plugin") version "0.5.217" apply false
+    id("org.jetbrains.fleet-plugin") version "0.5.223" apply false
 }
