ec8f85cf59408381fc43838a928112683cd2e706
Titouan Bion <titouan.bion@jetbrains.com>
2024-07-29 14:11:58 +0200

    Remove redundant repositories

--- sample-plugin/build.gradle.kts
+++ sample-plugin/build.gradle.kts
@@ -3,17 +3,13 @@ plugins {
     alias(libs.plugins.fleet.plugin)
 }
 
-repositories {
-    mavenCentral()
-}
-
 version = "0.1.0"
 
 fleetPlugin {
 
     // STEP 1: chose an ID for your plugin, it must be unique
     //
-    // id = "my.sample.plugin"
+    id = "my.sample.plugin"
 
     // STEP 2: set up the sensible metadata for your plugin
     // metadata {
