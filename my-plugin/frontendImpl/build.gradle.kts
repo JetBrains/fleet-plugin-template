ccf6f4e7d05fc79c54f2f68766a1786f98695659
Titouan Bion <titouan.bion@jetbrains.com>
2024-07-29 20:44:44 +0200

    Upgrade plugin version to 0.5.228`

--- my-plugin/frontendImpl/build.gradle.kts
+++ my-plugin/frontendImpl/build.gradle.kts
@@ -3,11 +3,6 @@ plugins {
     alias(libs.plugins.fleet.plugin.layer)
 }
 
-repositories {
-    mavenCentral()
-    maven("https://cache-redirector.jetbrains.com/intellij-dependencies") // needed to retrieve `rhizomedb-compiler-plugin` and `noria-compiler-plugin`
-}
-
 kotlin {
     jvmToolchain(17)
 }
